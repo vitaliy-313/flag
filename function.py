b88a7d711266ea0270a89f6dfccbf1df45087613
vitaliy-313 <witalik_pro_2016@mail.ru>
2023-10-30 10:48:02 +0500

    commit

--- function.py
+++ function.py
@@ -2,7 +2,10 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 import flask
 from flask import Flask, render_template, request, session, redirect
-
+import os
+app = Flask(__name__, template_folder='templates', static_folder='templates/static')
+app.secret_key = os.urandom(30).hex()
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 connect = sqlite3.connect('dbase.db', check_same_thread=False)
 cursor = connect.cursor()
 
@@ -59,7 +62,7 @@ def reg (login, password):
             cursor.execute('''INSERT INTO users('login', password) VALUES(?, ?)''', (login,hash))
             connect.commit()
             user = cursor.execute('''SELECT * FROM 'users' WHERE login = ?''', (login,)).fetchone()
-            # session['user_id'] = user[0]
+            session['user_id'] = user[0]
             return redirect('/profile', code=302)
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, session, redirect
-
+import os
 import function
 # from flask_jwt_extended import create_access_token, JWTManger, get_jwt_identity, jwt_required
 # import sqlite3, uuid, hashlib, random
@@ -8,7 +8,8 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from function import *
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/static')
-
+app.secret_key = os.urandom(30).hex()
+app.config['JWT_SECRET_KEY'] = 'super-secret'
 connect = sqlite3.connect('dbase.db', check_same_thread=False)
 cursor = connect.cursor()
 
@@ -16,8 +17,9 @@ cursor = connect.cursor()
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods =['GET', 'POST'])
 def login():
+
     if request.method == 'POST':
         login = request.form.get('login')
         passw = request.form.get('password')
@@ -26,11 +28,13 @@ def login():
 
 @app.route('/reg', methods =['GET', 'POST'])
 def reg():
+    session['user_id'] = ""
     if request.method == 'POST':
         login = request.form.get('login')
         passw = request.form.get('password')
         print(login, passw)
-        function.reg(login,passw)
+        function.reg(login, passw)
+        print(session['user_id'])
     else:
         print('незарегистрирован')
 
